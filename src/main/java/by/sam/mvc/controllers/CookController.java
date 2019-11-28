78571ca94681595c18c4222d6f91890a9b016cb0
egoshek <egorsheka@gmail.com>
2019-11-28 18:17:30 +0300

    spring security test config

--- src/main/java/by/sam/mvc/controllers/CookController.java
+++ src/main/java/by/sam/mvc/controllers/CookController.java
@@ -8,11 +8,25 @@ import by.sam.mvc.service.user.CookService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 public class CookController {
+
+    // Login form
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    // Login form with error
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
+    }
 
     @GetMapping(path = "/")
     public String getCookPersonalPage(Model model){
